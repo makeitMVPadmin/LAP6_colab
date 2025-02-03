6712af686aede9841d2dc5328f4550e74dec3d3c
snowball <25191617+f2xiao@users.noreply.github.com>
2025-02-03 12:01:15 -0500

    add return data interface to getAllGoalBuddies()

--- src/firebase/functions/goalBuddies.ts
+++ src/firebase/functions/goalBuddies.ts
@@ -1,17 +1,18 @@
 import { db } from '../firebase'
 import { collection, getDocs } from 'firebase/firestore'
+import { GoalBuddy, BaseGoalBuddy } from '../../utils/types'
 
 /*
  * Description: This function fetch all goal buddies in the collection.
  * @author [Fangfang]
  *
  */
-export const getAllGoalBuddies = async () => {
+export const getAllGoalBuddies = async (): Promise<GoalBuddy[]> => {
   try {
     const querySnapshot = await getDocs(collection(db, 'goal_buddies'))
     const goalBuddiesArray = querySnapshot.docs.map((doc) => ({
       id: doc.id,
-      ...doc.data(),
+      ...(doc.data() as BaseGoalBuddy),
     }))
     return goalBuddiesArray
   } catch (error) {
