19ed446e3bb076671acd8164af5b0252f8d030f7
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2025-02-03 12:33:45 -0500

    function to retrieve all data from firestore

--- src/firebase/functions/getAllCalendarEvents.ts
+++ src/firebase/functions/getAllCalendarEvents.ts
@@ -1,21 +1,19 @@
 // Get All Calendar events from database
 
-import { collection, getDocs } from "firebase/firestore"
-import { db } from "../firebase"
-import {baseEvents} from "../utils/types"
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import { baseEvents, calendarEvents } from "../utils/types";
 
-export const getAllCalendarEvents=async()=>{
-try{
-const querySnapshot=await getDocs(collection(db,'calendar_events'))
-const events =await querySnapshot.docs.map((doc)=>{
-    return{
-        id:doc.id ,
-        ...doc.data() as baseEvents
-    }
-})
-return events
-}
-catch(err){
-    console.error("Error fetching calendar events",err)
-}
-}
+export const getAllCalendarEvents = async (): Promise<calendarEvents[]> => {
+  try {
+    const querySnapshot = await getDocs(collection(db, 'calendar_events'));
+    const events = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...(doc.data() as baseEvents),
+    }));
+    return events;
+  } catch (err) {
+    console.error("Error fetching calendar events", err);
+    throw err; // Re-throw the error to handle it in the calling function
+  }
+};
