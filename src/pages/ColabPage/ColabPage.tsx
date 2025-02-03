579d5b39a6b89645916a486932c2a39787b8ce1a
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2025-02-03 13:45:30 -0500

    get events by id with firebase function

--- src/pages/ColabPage/ColabPage.tsx
+++ src/pages/ColabPage/ColabPage.tsx
@@ -1,14 +1,7 @@
-import { useEffect } from 'react'
-import { getByEventId } from '../../firebase/functions/getByEventId'
+
 
 export default function ColabPage() {
-  useEffect(() => {
-    const getbyId = async(eventId: string) => {
-      const events = await getByEventId(eventId)
-      console.log(events)
-    }
-    getbyId("1aM018MGW4YurFgjSr7m")
-  }, [])
+
   return (
     <main>
       <h1>Welcome Colab User</h1>
