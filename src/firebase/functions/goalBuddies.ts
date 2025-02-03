a284d8b863254bc1a3c23b59ed22b3d325ee23ec
snowball <25191617+f2xiao@users.noreply.github.com>
2025-02-02 20:18:52 -0500

    add comment to describe the function and it's author

--- src/firebase/functions/goalBuddies.ts
+++ src/firebase/functions/goalBuddies.ts
@@ -1,6 +1,11 @@
 import { db } from '../firebase'
 import { collection, getDocs } from 'firebase/firestore'
 
+/*
+ * Description: This function fetch all goal buddies in the collection.
+ * @author [Fangfang]
+ * @edited [Name of developer that edited this function after creation]
+ */
 export const getAllGoalBuddies = async () => {
   try {
     const querySnapshot = await getDocs(collection(db, 'goal_buddies'))
