f87e00d8d053efc819710e2b7652d5f9423ec006
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2025-02-03 12:29:11 -0500

    function to retrieve all data from firestore

--- src/firebase/functions/getAllCalendarEvents.ts
+++ src/firebase/functions/getAllCalendarEvents.ts
@@ -2,15 +2,15 @@
 
 import { collection, getDocs } from "firebase/firestore"
 import { db } from "../firebase"
-
+import {baseEvents} from "../utils/types"
 
 export const getAllCalendarEvents=async()=>{
 try{
 const querySnapshot=await getDocs(collection(db,'calendar_events'))
 const events =await querySnapshot.docs.map((doc)=>{
     return{
-        id:doc.id,
-        ...doc.data()
+        id:doc.id ,
+        ...doc.data() as baseEvents
     }
 })
 return events
---
+++ src/firebase/utils/types.ts
@@ -0,0 +1,14 @@
+export interface baseEvents {
+  createdAt: { seconds: Number; nanoseconds: Number }
+  createdUserId: string
+  invitedUserId: string
+  eventDescription: string
+  eventEndTime: { seconds: Number; nanoseconds: Number }
+  eventStartTime: { seconds: Number; nanoseconds: Number }
+  eventTitle: string
+  eventStatus: string
+  googleEventId: string
+}
+export interface calendarEvents extends baseEvents {
+  id: string
+}
