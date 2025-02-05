bfc08686d69c6ac0793b2996145168a606f01734
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2025-02-05 15:38:38 -0500

    created events by userid

--- firebase/functions/calendarEventsbyUserId.ts
+++ firebase/functions/calendarEventsbyUserId.ts
@@ -1,6 +1,9 @@
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 import { BaseEvents, CalendarEvents } from "../../src/types/types";
+/*
+Description:get All calendar Events by createdUserId 
+@author[Aparna]*/
 
 export async function getCalendarEventsbtCreatorId(createdUserId:string):Promise<CalendarEvents[]>{
     try{
@@ -19,4 +22,25 @@ export async function getCalendarEventsbtCreatorId(createdUserId:string):Promise
     }
     
 
+}
+
+/*
+Description: get All calendar Events by invitedUserId
+@author[Aparna]*/
+
+export async function getCalendarEventsbtInvitedId(invitedUserId:string):Promise<CalendarEvents[]>{
+    try{
+        const events=await collection(db,"calendar_events",invitedUserId);
+        const querySnapshot=await getDocs(events);
+        const calendarEventsArray:CalendarEvents[]=querySnapshot.docs.map((item)=>{
+          return{  id:item.id,
+            ...(item.data() as BaseEvents)
+          }
+        })
+        return calendarEventsArray;
+    }
+    catch(error){
+        console.error("Error getting documents: ", error);
+        return []
+    }
 }
