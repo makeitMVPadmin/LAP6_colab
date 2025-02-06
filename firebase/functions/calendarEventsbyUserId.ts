2cf6429fc2ca407f9db5c96988fa5b6b5ef62d5b
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2025-02-05 20:01:45 -0500

    created events by userid and inviteeid

--- firebase/functions/calendarEventsbyUserId.ts
+++ firebase/functions/calendarEventsbyUserId.ts
@@ -5,9 +5,9 @@ import { BaseEvents, CalendarEvents } from "../../src/types/types";
 Description:get All calendar Events by UserIds 
 @author[Aparna]*/
 
-export async function getCalendarEventsbyUserIds(createdUserId:string ,inviteeUserId:string):Promise<CalendarEvents[]>{
+export async function getCalendarEventsbyUserIds(createdUserId:string,inviteeUserId:string,):Promise<CalendarEvents[]>{
     try{
-        const events=query(collection(db,"calendar_events"),where("createdUserId","==",createdUserId && "inviteeUserId","==",inviteeUserId)) ;
+        const events=query(collection(db,"calendar_events"),where("createdUserId","==",createdUserId),where("invitedUserId","==",inviteeUserId)) ;
         const querySnapshot=await getDocs(events);
         const calendarEventsArray:CalendarEvents[]=querySnapshot.docs.map((item)=>{
           return{  
@@ -25,23 +25,3 @@ export async function getCalendarEventsbyUserIds(createdUserId:string ,inviteeUs
 
 }
 
-/*
-Description: get All calendar Events by invitedUserId
-@author[Aparna]*/
-
-export async function getCalendarEventsbyInviteeId(invitedUserId:string):Promise<CalendarEvents[]>{
-    try{
-        const events=query(collection(db,"calendar_events"),where("invitedUserId","==",invitedUserId)) ;
-        const querySnapshot=await getDocs(events);
-        const calendarEventsArray:CalendarEvents[]=querySnapshot.docs.map((item)=>{
-          return{  id:item.id,
-            ...(item.data() as BaseEvents)
-          }
-        })
-        return calendarEventsArray;
-    }
-    catch(error){
-        console.error("Error getting documents: ", error);
-        return []
-    }
-}
