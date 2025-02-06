e8b61c8f7990161e2c3e688cf138f2f457f6d3a8
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2025-02-06 14:38:26 -0500

    creating data for database

--- firebase/functions/createCalendarEvent.ts
+++ firebase/functions/createCalendarEvent.ts
@@ -1,9 +1,31 @@
-export async function addCalendarEvent():Promise<CalendarEvents[]>{
-try{
-const event=await addDoc(collection(db,"calendar_events"))
-}
-catch(error){
-    console.error("Error creating document: ", error);
-    return []
-}
+import { addDoc, collection, Timestamp } from 'firebase/firestore'
+import { db } from '../firebase'
+export async function addCalendarEvent(
+  createdUserId: string,
+  eventDescription: string,
+  eventStartTime: Date,
+  eventEndTime: Date,
+  eventTitle: string,
+  invitedUserId: string,
+  eventStatus: 'pending' | 'completed' | 'canceled',
+  googleEventId: string,
+): Promise<Object> {
+  try {
+    const event = await addDoc(collection(db, 'calendar_events'), {
+      createdAt: Timestamp.fromDate(new Date()),
+      updatedAt: Timestamp.fromDate(new Date()),
+      createdUserId: createdUserId,
+      eventDescription: eventDescription,
+      eventStartTime: Timestamp.fromDate(eventStartTime),
+      eventEndTime: Timestamp.fromDate(eventEndTime),
+      eventTitle: eventTitle,
+      invitedUserId: invitedUserId,
+      eventStatus: eventStatus,
+      googleEventId: googleEventId,
+    })
+    return event
+  } catch (error) {
+    console.error('Error creating document: ', error)
+    return {}
+  }
 }
