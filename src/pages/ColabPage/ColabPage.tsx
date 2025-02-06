667fbabde2f918d821eeb68fd9381073f6644359
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2025-02-05 20:18:21 -0500

    created events by userid and inviteeid

--- src/pages/ColabPage/ColabPage.tsx
+++ src/pages/ColabPage/ColabPage.tsx
@@ -1,17 +1,8 @@
 import DummyNavBar from '@/components/DummyNavBar/DummyNavBar'
 import GoalBuddyCard from '@/components/GoalBuddyCard/GoalBuddyCard'
-import { useEffect } from 'react'
-import {getCalendarEventsbyUserIds} from "../../../firebase/functions/calendarEventsbyUserId"
+
 export default function ColabPage() {
-  useEffect(() => {
-    const fetchCalendarEventsByUserId = async (createdId: string,inviteeId:string) => {
-    const response = await getCalendarEventsbyUserIds(createdId,inviteeId)
-    console.log(response)
-    }
-   
-    fetchCalendarEventsByUserId('rOkIPaJbpDxHABmnPGTN','RwXczeoWBfq4tEJ5BKoY')
-    
-    }, [])
+  
 
   return (
     <main>
