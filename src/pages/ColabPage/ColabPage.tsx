1ec145509c1f124394bc050ed97f39b4784c1b0c
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2025-02-07 13:32:05 -0500

    refactoring the code by get All calendar Events by either createdUserIds or inviteeUserIds and we should filter events to only include those that occur on or after the specified date.

--- src/pages/ColabPage/ColabPage.tsx
+++ src/pages/ColabPage/ColabPage.tsx
@@ -1,27 +1,10 @@
 
 import DummyNavBar from '@/components/DummyNavBar/DummyNavBar'
 import GoalBuddyCard from '@/components/GoalBuddyCard/GoalBuddyCard'
-import { useEffect } from 'react';
-import {Timestamp} from 'firebase/firestore';
 
-import { getUserEvents } from '../../../firebase/functions/calendarEventsbyUserIds';
 
 export default function ColabPage() {
-  useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const userId = "rOkIPaJbpDxHABmnPGTN";
-       const date = Timestamp.fromDate(new Date(Date.UTC(2025, 1, 10, 16, 0, 0))) 
-        const userEvents = await getUserEvents(userId,date);
-        console.log(userEvents);
-      } catch (error) {
-        console.error('Failed to fetch events: ', error);
-      }
-    };
-
-    fetchEvents();
-  }, []);
-
+  
   return (
     <main>
      <DummyNavBar />
