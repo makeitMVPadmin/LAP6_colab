0f26d7bdb536af2b71bff05115d882426470c3c4
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2025-02-09 15:59:15 -0500

    create calendar events

--- src/pages/ColabPage/ColabPage.tsx
+++ src/pages/ColabPage/ColabPage.tsx
@@ -1,24 +1,10 @@
-import { createCalendarEvent,EventData} from '../../../firebase/functions/createCalendarEvent'
+
 export default function ColabPage() {
-  const handleClick = async () => {
-    const eventData:EventData = {
-      createdUserId: 'yLVEZkRulVkRU8de8XMb',
-      eventDescription: 'Team collaboration fix the bugs',
-      eventStartTime: new Date('2025-02-11T11:00:00'),
-      eventEndTime: new Date('2025-02-11T11:30:00'),
-      eventTitle: 'Team meeting to ensure reliabality',
-      invitedUserId: 'RwXczeoWBfq4tEJ5BKoY',
-      eventStatus: 'confirmed',
-      googleEventId: 'google-12346',
-    }
-    const response = await createCalendarEvent(eventData)
-    console.log(response)
-  }
+ 
 
   return (
     <div>
-      <h1>Welcome Colab User</h1>
-      <button onClick={handleClick}>Add Data</button>
+      <h1>Welcome to Colab user</h1>
     </div>
   )
 }
