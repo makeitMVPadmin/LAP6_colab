46d8befe6d4be279654a93284bd810abfcf9001f
klkode <katrinalkite@gmail.com>
2025-02-10 11:25:45 -0500

    Adding different displays for no date selected and no availabilities for that date.

--- src/components/TimeSelectionList/TimeSelectionList.tsx
+++ src/components/TimeSelectionList/TimeSelectionList.tsx
@@ -8,31 +8,39 @@ interface TimeSelectionListProps {
 }
 
 const TimeSelectionList: React.FC<TimeSelectionListProps> = ({timesList, selectedDate, setTime}) => {
-  const [activeRow, setActiveRow] = useState<number | null>(null);
-  console.log("provided timesList: ");
-  console.log(timesList);
+    const [activeRow, setActiveRow] = useState<number | null>(null);
+    console.log("provided timesList: ");
+    console.log(timesList);
 
-  return (
-    <div className="p-5 max-h-[500px] bg-white border-2 border-black rounded-lg">
-      <div className="sticky top-0 bg-white z-10 p-2 border-b border-gray-600">
-        <span className="font-bold">{selectedDate?.toDateString()}</span>
-      </div>
-      <div className="overflow-auto scrollbar-hide max-h-[150px]">
-        {timesList.map((time: TimePeriod, index: number) => (
-          <div
-            key={index}
-            className={`flex items-center mb-2 border-b border-gray-600 pb-2 ${
-              activeRow === index + 1 ? "bg-[#EEEEEE]" : "hover:bg-gray-200"
-            }`}
-            onClick={() => setActiveRow(index + 1)}
-          >
-            <span className="mr-2">{activeRow === index + 1 ? "✔" : ""}</span>
-            <span className="text-lg">{time.startTime.hours + ":" + time.startTime.minutes + " - " + time.endTime.hours + ":"+ time.endTime.minutes}</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        selectedDate === undefined ? (
+            <div>No Date Selected</div>
+        ) : (
+            <div className="p-5 max-h-[500px] bg-white border-2 border-black rounded-lg">
+                <div className="sticky top-0 bg-white z-10 p-2 border-b border-gray-600">
+                    <span className="font-bold">{selectedDate.toDateString()}</span>
+                </div>
+                {timesList.length === 0 ? (
+                    <div>No Availability for this Date</div>
+                ) : (
+                    <div className="overflow-auto scrollbar-hide max-h-[150px]">
+                        {timesList.map((time: TimePeriod, index: number) => (
+                            <div
+                            key={index}
+                            className={`flex items-center mb-2 border-b border-gray-600 pb-2 ${
+                            activeRow === index + 1 ? "bg-[#EEEEEE]" : "hover:bg-gray-200"
+                            }`}
+                            onClick={() => setActiveRow(index + 1)}
+                            >
+                                <span className="mr-2">{activeRow === index + 1 ? "✔" : ""}</span>
+                                <span className="text-lg">{time.startTime.hours + ":" + time.startTime.minutes + " - " + time.endTime.hours + ":"+ time.endTime.minutes}</span>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        )
+    );
 };
 
 export default TimeSelectionList;
