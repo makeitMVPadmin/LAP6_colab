74a149e85b3e62bf2e4f54f653811aff9ff48ae7
klkode <katrinalkite@gmail.com>
2025-02-10 16:43:53 -0500

    Reverted ColabPage.tsx to it' original state as the test environment should not be pushed to develop.

--- src/pages/ColabPage/ColabPage.tsx
+++ src/pages/ColabPage/ColabPage.tsx
@@ -1,60 +1,13 @@
-import MeetingSetupSection from "@/components/MeetingSetupSection/MeetingSetupSection";
 import DummyNavBar from '@/components/DummyNavBar/DummyNavBar'
 import GoalBuddyCard from '@/components/GoalBuddyCard/GoalBuddyCard'
-import { useEffect, useState } from "react";
-import { getGoalBuddyById } from '../../../firebase/functions/getGoalBuddyById';
-import { GoalBuddy } from "@/types/types";
 
 export default function ColabPage() {
-
-  const [goalBuddy, setGoalBuddy] = useState<GoalBuddy | undefined>(undefined);
-  const [loading, setLoading] = useState<boolean>(true);
-
-
-  useEffect(() => {
-    const fetchOneGoalBuddyById = async (id: string) => {
-      setLoading(true);
-      try{
-        const data = await getGoalBuddyById(id);
-        if(data === null){
-          setGoalBuddy(undefined);
-        }else{
-          setGoalBuddy(data as GoalBuddy);
-        }
-      }
-      catch(error){
-        console.error('Failed to fetch goal buddy: ', error);
-        setGoalBuddy(undefined);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchOneGoalBuddyById('0u0Ibxw1kYuiREJGKlZk')
-  }, []);
-
-  if (loading) {
-    return (
-      <main>
-        <DummyNavBar />
-        <div className="flex justify-center">
-          <p>Loading...</p>
-        </div>
-      </main>
-    )
-  }
-  
- return (
-   <main>
-    <DummyNavBar />
-     <div className="flex justify-center">
-       <GoalBuddyCard goalBuddyList={[]}/>
-     </div>
-     {goalBuddy === undefined ? (
-        <div>No Goal Buddy found</div>
-      ) : (
-        <MeetingSetupSection activeUserId="6vEljF3oaFsLWEPmQRHV" showingUser={goalBuddy} />
-      )}
-   </main>
- )
+  return (
+    <main>
+      <DummyNavBar />
+      <div className="flex justify-center">
+        <GoalBuddyCard goalBuddyList={[]}/>
+      </div>
+    </main>
+  )
 }
