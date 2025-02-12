f55797f8b3d3f459638d8d49486393e08240fadd
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2025-02-12 14:00:18 -0500

    Creating Functions for editing goalbuddy info

---
+++ firebase/functions/editGoalBuddyBioAndInterest.ts
@@ -0,0 +1,9 @@
+export const editGoalBuddyBioAndInterest(userId:string){
+    try{
+        const goalBuddy
+    }
+catch(error){
+    console.error('Failed to edit Goal Buddy Bio and Interests', error)
+    throw new Error('Failed to edit Goal Buddy Bio and Interests')
+}
+}
