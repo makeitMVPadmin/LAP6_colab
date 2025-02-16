6646d9a6c3d8fdd03221a87bff97a8152aac84d3
Redi-Molla <redietmolla8@gmail.com>
2025-02-16 15:11:43 -0500

    Added profile function to the nav bar avatar; deleted search icon; removed search bar, menu bar, and navigation links to match hi-fi wireframe

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,54 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Avatar, AvatarFallback } from '../ui/avatar'
 import CommunitiLogo from '../../assets/images/communiti.png'
-import SearchIcon from '../../assets/images/Search Icon.svg'
-import { FiMenu } from 'react-icons/fi'
+import { SidebarContext } from '@/components/context/SidebarContext'
+import { AppSidebar } from '../AppSidebar/AppSidebar'
+import clsx from 'clsx'
 
 const NavBar: React.FC = () => {
+  const sideBarContext = useContext(SidebarContext)
+  if (!sideBarContext) {
+    throw new Error('Sidebar context not found')
+  }
+  const { isSidebarOpen, setIsSideBarOpen } = sideBarContext
+  const handleClick = () => {
+    setIsSideBarOpen(!isSidebarOpen)
+  }
   return (
     <nav
-      className="flex flex-col p-6 shadow-md h-30"
-      style={{ backgroundColor: '#AFACAC' }}
+      className={clsx(
+        'flex flex-col mb-2 shadow-md h-[100px]',
+        isSidebarOpen && 'z-0',
+      )}
+      // style={{ backgroundColor: '#AFACAC' }}
     >
-      <div className="flex items-center justify-between w-full ">
+      <div className="flex items-center justify-between w-full p-6">
         {/* Logo and Hamburger Menu */}
-        <div className="flex items-center justify-end space-x-5">
-          <FiMenu viewBox="0 0 20 20" className="text-4xl" />
+        <div className="flex items-center justify-end ">
           <img src={CommunitiLogo} alt="Communiti Logo" className="h-10" />
         </div>
 
         {/* Search and Avatar */}
-        <div className="flex items-center space-x-6">
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="Search"
-              className="px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 w-75"
-            />
-            <img
-              src={SearchIcon}
-              alt="Search Icon"
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 h-5 w-5"
-            />
-          </div>
-          <Avatar className="w-10 h-10">
+        <div className="flex items-center">
+          <Avatar className="w-10 h-10 cursor-pointer" onClick={handleClick}>
             <AvatarFallback className="bg-gray-200"></AvatarFallback>
           </Avatar>
         </div>
       </div>
 
-      {/* Navigation Links */}
-      <div className="flex space-x-6 mt-4 ml-12">
-        <a href="#" className="text-gray-700 hover:text-gray-900">
-          Who We Are
-        </a>
-        <a href="#" className="text-gray-700 hover:text-gray-900">
-          What We Do
-        </a>
-        <a href="#" className="text-gray-700 hover:text-gray-900">
-          Join the Community
-        </a>
-      </div>
+      {isSidebarOpen && <AppSidebar />}
     </nav>
   )
 }
