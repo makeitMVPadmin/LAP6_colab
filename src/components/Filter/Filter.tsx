38ddee3a8fb659aa1b0a15f8605bb409c18e11a4
Kevin Meisenbacher <kevinmeisenbacher@gmail.com>
2025-02-15 22:25:41 -0500

    Cleaned up filter component and put role badges in their own component for reusability

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,6 +1,7 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card'
+import { Card, CardContent, CardDescription, CardHeader } from '../ui/card'
 import { cn } from "../lib/utils"
 import { Checkbox } from '../ui/checkbox'
+import RoleBadge from './RoleBadge'
 
 interface filterProps {
     filterGoalBuddies: Function,
@@ -15,46 +16,45 @@ const Filter: React.FC<filterProps> = ({ filterGoalBuddies, filter }) => {
     return (
         <Card className={cn("w-64 h-44 pb-0 border-black border-b-2 border-r-2")}>
             <CardHeader className={cn("pt-2 pr-4")}>
-            <CardDescription 
-                className={cn("text-right text-sm font-light m-0 cursor-pointer")}
-                onClick={() => filterGoalBuddies('')}>Clear All
-            </CardDescription>
+                <CardDescription 
+                    className={cn("text-right font-light m-0 cursor-pointer")}
+                    onClick={() => filterGoalBuddies('')}>Clear All
+                </CardDescription>
             </CardHeader>
             <CardContent className={cn("flex flex-col gap-4 pr-4 pl-4 pb-0")}>
                 <div className="flex flex-row justify-between relative">
-                    <label className="p-0">
-                        <span className="h-3 w-3 mr-1 inline-block align-center bg-blue-600"></span>
+                    <label>
+                        <RoleBadge colour={"bg-blue-600"} />
                         Mentor
                     </label>
                     <Checkbox 
                         className={cn("h-5 w-5 border-2 border-slate-600 rounded-sm")}
                         checked={filter.mentor}
-                        onClick={() => {filterGoalBuddies('mentor')}}>
-                    </Checkbox>
+                        onClick={() => {filterGoalBuddies('mentor')}}
+                    />
                 </div>
                 <div className="flex flex-row justify-between relative">
-                    <label className="p-0">
-                        <span className="h-3 w-3 mr-1 inline-block align-center bg-orange-600"></span>
-                        Goal Buddy
+                    <label>
+                        <RoleBadge colour={"bg-orange-600"} />Goal Buddy
                     </label>
                     <Checkbox 
                         className={cn("h-5 w-5 border-2 border-slate-600 rounded-sm")}
                         checked={filter.accountability}
-                        onClick={() => {filterGoalBuddies('accountability')}}>
-                    </Checkbox>
+                        onClick={() => {filterGoalBuddies('accountability')}}
+                    />
                 </div>
                 <div className="flex flex-row justify-between">
-                    <label className="p-0">
-                        <span className="h-3 w-3 mr-1 inline-block align-center bg-green-600"></span>
+                    <label>
+                    <RoleBadge colour={"bg-green-600"} />
                         Networking
                     </label>
                     <Checkbox 
                         className={cn("h-5 w-5 border-2 border-slate-600 rounded-sm")}
                         checked={filter.networking}
-                        onClick={() => {filterGoalBuddies('networking')}}>
-                    </Checkbox>
+                        onClick={() => {filterGoalBuddies('networking')}}
+                    />
                 </div>
-                </CardContent>
+            </CardContent>
         </Card>
     )
 }
