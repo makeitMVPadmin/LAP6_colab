c51e149187fd74a397b3dbcf4559e82c8d4d2e5a
KevinMeisenbacher <55214897+KevinMeisenbacher@users.noreply.github.com>
2025-02-24 11:01:41 -0500

    Removed redundant code and matched sizes to Figma to the wireframe to the letter with some adjustments to make it look visually closer

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardDescription, CardHeader } from '../ui/card'
+import { Card, CardContent, CardDescription } from '../ui/card'
 import { cn } from "../lib/utils"
 import MentorBadge from './MentorBadge'
 import GoalBuddyBadge from './GoalBuddyBadge'
@@ -15,52 +15,42 @@ interface filterProps {
 }
 
 const Filter: React.FC<filterProps> = ({ filterGoalBuddies, filter }) => {
-    
-    
-    const renderCheckbox = (bool: boolean, role: string) => {
+    const renderCheckbox = (filtered: boolean, role: string) => {
         return (
             <span 
-                className={cn(`cursor-pointer h-5 w-5 rounded-sm border-2 border-black self-center ml-4 ${bool
+                className={cn(`cursor-pointer h-5 w-5 rounded-sm border-2 border-black self-center ml-4 ${filtered
                     ? "bg-black" : "bg-white"
                 }`)}
                 onClick={() => {filterGoalBuddies(role)}}
             />
         )
     }
-    
-    const bounds = {
-        card: "rounded-lg border border-r-2 border-b-2 border-black w-44 pl-2 relative",
-        role: "flex flex-row justify-between relative h-8",
-        cardHeader: "font-bold text-lg p-0"
-    } 
 
-    const renderRole = (tag: string, label: string) => {
+    const renderRole = (tag: string, roleName: string, roleToFilter: boolean) => {
         const smallBadge = (
-            tag === 'mentor' && <MentorBadge width="w-5" stroke="2" /> ||
-            tag === 'goalBuddy' && <GoalBuddyBadge width="w-5" stroke="2" /> ||
-            tag === 'networking' && <NetworkingBadge width="w-5" stroke="2" />
+            tag === 'mentor' && <MentorBadge width="w-8" stroke="2" /> ||
+            tag === 'goalBuddy' && <GoalBuddyBadge width="w-8" stroke="2" /> ||
+            tag === 'networking' && <NetworkingBadge width="w-8" stroke="2" />
         )
         
         return (
-            <div className={bounds.role}>
-                <label 
-                    className={bounds.card}
-                >
-                    {smallBadge} {label}
+            <div className="flex flex-row justify-between relative h-8">
+                <label className="rounded-lg border-2 border-r-4 border-b-4 border-black w-48 h-9 pl-2 text-xl leading-[30px] relative font[Montserrat] font-medium">
+                    {smallBadge} {roleName}
                 </label>
-                {renderCheckbox(filter.mentor, 'mentor')}
+                {renderCheckbox(roleToFilter, tag)}
             </div>
         )
     }
 
     return (
-        <Card className={cn("lg:w-60 h-16 lg:h-44 mt-4 ml-4 pb-0 border-none shadow-none")}>
+        <Card className={cn("lg:w-68 h-16 lg:h-44 mt-4 ml-4 pb-0 border-none shadow-none")}>
             <CardContent className={cn("flex flex-row justify-between lg:flex-col gap-4 p-0 md:pr-4 md:pl-4")}>
-                {renderRole('mentor', 'Mentor')}
-                {renderRole('goalBuddy', 'Goal Buddy')}
-                {renderRole('networking', 'Networking')}
+                {renderRole('mentor', 'Mentor', filter.mentor)}
+                {renderRole('goalBuddy', 'Goal Buddy', filter.accountability)}
+                {renderRole('networking', 'Networking', filter.networking)}
                 <CardDescription 
-                    className={cn("text-right text-black text-md m-0 -mt-3 cursor-pointer")}
+                    className={cn("text-right text-gray-800 text-base font[Montserrat] font-medium m-0 -mt-3 cursor-pointer")}
                     onClick={() => filterGoalBuddies('')}>Clear All
                 </CardDescription>
             </CardContent>
