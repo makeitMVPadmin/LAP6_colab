155b606e7e5b2e457be5bbfe0493975c13e7c19f
snowball <25191617+f2xiao@users.noreply.github.com>
2025-02-24 14:42:06 -0500

    remove TooltipContentWrapper and add the content to TooltipWrapper component

--- src/components/Tooltip/TooltipWrapper.tsx
+++ src/components/Tooltip/TooltipWrapper.tsx
@@ -6,17 +6,19 @@ import {
 } from '@/components/ui/tooltip'
 
 interface TooltipWrapperProps {
-  content: React.ReactNode
+  roleItem: Record<string, string>
   children: React.ReactNode
 }
 
-export function TooltipWrapper({ content, children }: TooltipWrapperProps) {
+export function TooltipWrapper({ roleItem, children }: TooltipWrapperProps) {
   return (
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>{children}</TooltipTrigger>
-        <TooltipContent className="text-black rounded-md border-t border-t-black border-r-2 border-r-black border-b-2 border-b-black border-l border-l-black bg-white absolute ml-36 top-1/2 transform -translate-y-1/2">
-          {content}
+        <TooltipContent className="flex flex-col items-center p-4 gap-2 w-[390px] h-[120] text-black rounded-md border-t border-t-black border-r-2 border-r-black border-b-2 border-b-black border-l border-l-black bg-white absolute left-36 top-1/2 transform -translate-y-1/2">
+          <img className="w-[30px]" src={roleItem.icon} alt="mentor-icon" />
+          <p className="font-semibold text-[20px]">{roleItem.role}</p>
+          <p className="text-center text-[16px]">{roleItem.description}</p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
