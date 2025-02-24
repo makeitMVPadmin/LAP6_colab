65c6ea5be5c27a82cccc883f637ddf79fdc133d2
Jeffrey Chu <jeffcolinchu@gmail.com>
2025-02-24 16:12:48 -0500

    replaced rolebadge with new badge components and tweaked CSS

--- src/components/GoalBuddyCard/GoalBuddyCard.tsx
+++ src/components/GoalBuddyCard/GoalBuddyCard.tsx
@@ -4,7 +4,9 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { AllGoalBuddyData } from '@/types/types'
 import { SidebarContext } from '../context/SidebarContext'
 import clsx from 'clsx'
-import RoleBadge from '../Filter/RoleBadge'
+import MentorBadge from '../Filter/MentorBadge'
+import GoalBuddyBadge from '../Filter/GoalBuddyBadge'
+import NetworkingBadge from '../Filter/NetworkingBadge'
 
 interface GoalBuddyCardProps {
   goalBuddy: AllGoalBuddyData
@@ -22,35 +24,36 @@ const GoalBuddyCard: React.FC<GoalBuddyCardProps> = ({
   const { isSidebarOpen } = sideBarContext
   return (
     <Card
-      key={goalBuddy.userId} onClick={onClick}
+      key={goalBuddy.userId}
+      onClick={onClick}
       className={clsx(
         'flex min-w-60 max-w-96 min-h-[150px] m-4 bg-white cursor-pointer duration-150 border border-slate-950 rounded-md shadow-[2px_2px_5px_0px_rgba(0,_0,_0,_0.8)]',
-        !isSidebarOpen && 'hover:scale-110', // Disable hover when sidebar is open
+        !isSidebarOpen && 'hover:scale-105', // Disable hover when sidebar is open
         isSidebarOpen && ' opacity-110', // Add transparency when sidebar is open
       )}
     >
-      <CardHeader className="self-start">
-        <Avatar className="w-16 h-16">
+      <CardHeader className="self-start p-3 sm:p-6">
+        <Avatar className="sm:w-16 sm:h-16">
           <AvatarImage src={goalBuddy.profilePhoto} />
           <AvatarFallback className="bg-[#D9D9D9]" />
         </Avatar>
       </CardHeader>
       <CardContent className="flex-1 flex-col justify-center p-4 w-11/12 relative">
-        <div className="flex justify-end absolute top-0 right-0 p-2 mx-1 ">
-          {goalBuddy.isMentor && <RoleBadge colour={'bg-[#0264D4]'} />}
+        <div className="flex justify-end items-center absolute top-0 right-0 px-2 py-1 gap-1">
+          {goalBuddy.isMentor && <MentorBadge width="w-4" stroke="3" />}
           {goalBuddy.isAccountabilityPartner && (
-            <RoleBadge colour={'bg-[#FFA629]'} />
+            <GoalBuddyBadge width="w-4" stroke="3" />
           )}
-          {goalBuddy.isNetworking && <RoleBadge colour={'bg-[#4CAF50]'} />}
+          {goalBuddy.isNetworking && <NetworkingBadge width="w-4" stroke="3" />}
         </div>
-        <h1 className="font-semibold text-lg sm:text-xl md:text-2xl mt-2">{`${goalBuddy.firstName} ${goalBuddy.lastName}`}</h1>
-        <span className="text-sm sm:text-base">{`${goalBuddy.discipline}`}</span>
+        <h1 className="font-semibold text-lg sm:text-xl md:text-2xl mt-2 font-fraunces">{`${goalBuddy.firstName} ${goalBuddy.lastName}`}</h1>
+        <span className="text-sm sm:text-base font-montserrat">{`${goalBuddy.discipline}`}</span>
         {goalBuddy.interests.length > 0 && (
           <div className="text-sm sm:text-base flex flex-wrap gap-1.5 mt-2">
             {goalBuddy.interests.map((interest, index) => (
               <span
                 key={index}
-                className="bg-gray-200 list-inside text-sm sm:text-base rounded-md px-1"
+                className="bg-gray-200 list-inside text-sm sm:text-base rounded-md px-1 font-montserrat"
               >
                 #{interest}
               </span>
