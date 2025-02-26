be64a4d9a87ebb4798a9287b9bbc8a138660bda1
Redi-Molla <redietmolla8@gmail.com>
2025-02-26 18:27:02 -0500

    Make the nav bar responsive

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -14,21 +14,20 @@ const NavBar: React.FC = () => {
     navigate('/', { replace: true })
     window.location.reload()
   }
-const sideBarContext = useContext(SidebarContext)
-const idContext=useContext(IdContext)
-if (!sideBarContext) {
-  throw new Error('Sidebar context not found')
-}
-const { isSidebarOpen, setIsSideBarOpen } = sideBarContext
-const userProfileData = idContext?.userData
+  const sideBarContext = useContext(SidebarContext)
+  const idContext = useContext(IdContext)
+  if (!sideBarContext) {
+    throw new Error('Sidebar context not found')
+  }
+  const { isSidebarOpen, setIsSideBarOpen } = sideBarContext
+  const userProfileData = idContext?.userData
 
-if (!userProfileData) {
-  return null
-
-}
-const handleClick = () => {
-  setIsSideBarOpen(!isSidebarOpen)
-}
+  if (!userProfileData) {
+    return null
+  }
+  const handleClick = () => {
+    setIsSideBarOpen(!isSidebarOpen)
+  }
   return (
     <nav
       className={clsx(
@@ -42,12 +41,19 @@ const handleClick = () => {
           className="flex items-center justify-end"
           onClick={handleLogoClick}
         >
-          <img src={CommunitiLogo} alt="Communiti Logo" className="h-10" />
+          <img
+            src={CommunitiLogo}
+            alt="Communiti Logo"
+            className="h-5 lg:h-10 md:h-7"
+          />
         </div>
 
         {/*Avatar (opens profile sidebar) */}
         <div className="flex items-center">
-          <Avatar className="w-10 h-10 cursor-pointer" onClick={handleClick}>
+          <Avatar
+            className="h-7 w-7 md:w-10 md:h-10 cursor-pointer"
+            onClick={handleClick}
+          >
             <AvatarFallback className="bg-gray-200"></AvatarFallback>
             <AvatarImage
               className="w-full"
