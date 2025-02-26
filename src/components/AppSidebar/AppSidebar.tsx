f4a6a24cb0ff25e8b4d27a96fc89ef84b2b2aca6
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2025-02-26 01:35:30 -0500

    fixing style in useeprofile  modal left

--- src/components/AppSidebar/AppSidebar.tsx
+++ src/components/AppSidebar/AppSidebar.tsx
@@ -46,8 +46,8 @@ export function AppSidebar() {
   return (
     <>
    <div className="w-screen max-h-screen relative z-50">
-        <Sidebar className="w-[20%] absolute h-[70%] top-[1%] right-7 shadow-md border border-[2px] border-gray-600">
-          <SidebarContent className="gap-0">
+        <Sidebar className="w-[20%] rounded-md absolute h-[70%] top-[1%] right-7 shadow-md border border-[2px] border-gray-600">
+          <SidebarContent className="gap-0 rounded-md">
             <div className="h-[50%] bg-yellow">
               <button
                 className={clsx(
