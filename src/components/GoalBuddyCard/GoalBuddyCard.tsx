42f792768a4c1e0c88a9333c1c5a6b09fce1e88d
Jeffrey Chu <jeffcolinchu@gmail.com>
2025-02-27 14:08:49 -0500

    worked on PR comments for font color and alignment

--- src/components/GoalBuddyCard/GoalBuddyCard.tsx
+++ src/components/GoalBuddyCard/GoalBuddyCard.tsx
@@ -32,24 +32,30 @@ const GoalBuddyCard: React.FC<GoalBuddyCardProps> = ({
         'flex flex-col sm:flex-row min-h-32 max-w-80 mx-2 my-2 sm:my-4 pb-2 px-5 pt-3 sm:py-2 sm:px-3 bg-white cursor-pointer duration-150 border border-slate-950 border-r-2 border-b-2 rounded-md relative',
         !isSidebarOpen && 'hover:scale-105', // Disable hover when sidebar is open
 
-        modalState ||isSidebarOpen ? " fade-in-0 duration-200 opacity-50 bg-opacity-50" : ""
-
+        modalState || isSidebarOpen
+          ? ' fade-in-0 duration-200 opacity-50 bg-opacity-50'
+          : '',
       )}
     >
       <div className="flex justify-end absolute top-0 right-0 gap-1 py-1 px-2">
         {goalBuddy.isMentor && (
           <div className="flex-grow flex-basis-1/3">
-            <MentorBadge width="w-4" stroke="3" yOffset={35} />
+            <MentorBadge
+              width="w-[15px]"
+              stroke="4"
+              yOffset={35}
+              height="h-[15px]"
+            />
           </div>
         )}
         {goalBuddy.isAccountabilityPartner && (
           <div className="flex-grow flex-basis-1/3">
-            <GoalBuddyBadge width="w-4" stroke="3" />
+            <GoalBuddyBadge width="w-[15px]" stroke="3" height="h-[15px]" />
           </div>
         )}
         {goalBuddy.isNetworking && (
           <div className="flex-grow flex-basis-1/3">
-            <NetworkingBadge width="w-4" stroke="3" />
+            <NetworkingBadge width="w-[15px]" stroke="3" height="h-[15px]" />
           </div>
         )}
       </div>
@@ -60,14 +66,14 @@ const GoalBuddyCard: React.FC<GoalBuddyCardProps> = ({
         </Avatar>
       </CardHeader>
       <CardContent className="flex sm:flex-1 flex-col justify-center mt-3 sm:mt-0 pt-3 p-0 sm:pt-6 sm:px-3 pb-0 relative">
-        <h1 className="font-semibold text-lg leading-[20px] sm:text-[24px] sm:leading-5 font-fraunces">{`${goalBuddy.firstName} ${goalBuddy.lastName}`}</h1>
-        <span className="text-sm font-medium font-montserrat">{`${goalBuddy.discipline}`}</span>
+        <h1 className="font-semibold text-lg leading-[20px] sm:text-[24px] sm:leading-5 font-fraunces text-foreground">{`${goalBuddy.firstName} ${goalBuddy.lastName}`}</h1>
+        <span className="text-sm font-medium font-montserrat  text-foreground">{`${goalBuddy.discipline}`}</span>
         {goalBuddy.interests.length > 0 && (
           <div className="flex flex-wrap gap-x-1">
             {goalBuddy.interests.map((interest, index) => (
               <span
                 key={index}
-                className={`list-inside text-sm font-medium rounded-md font-montserrat ${modalState ? ' fade-in-0 duration-200 brightness-50 bg-opacity-50' : ''}`}
+                className={`list-inside text-sm font-medium rounded-md font-montserrat text-foreground ${modalState ? ' fade-in-0 duration-200 brightness-50 bg-opacity-50' : ''}`}
               >
                 #{interest}
               </span>
