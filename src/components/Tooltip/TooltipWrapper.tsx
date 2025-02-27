1d4b05ec657b0eccf41785b1f3363d0b12332443
Kevin Meisenbacher <kevinmeisenbacher@gmail.com>
2025-02-26 21:48:26 -0500

    Removed all traces of the modal state and butted the tooltips right up against the filter cards

--- src/components/Tooltip/TooltipWrapper.tsx
+++ src/components/Tooltip/TooltipWrapper.tsx
@@ -17,7 +17,7 @@ export function TooltipWrapper({ roleItem, children }: TooltipWrapperProps) {
         <TooltipTrigger asChild>{children}</TooltipTrigger>
         <TooltipContent className={`flex flex-col items-center p-5 gap-2 w-[391px] h-[120] text-black 
           rounded-md border-t border-black border-r-2 border-b-2 border-l overflow-visible
-          bg-white absolute left-[150px] -top-[42px] transition duration-200 fade-in-0`}>
+          bg-white absolute left-[105px] -top-[42px] transition duration-200 fade-in-0`}>
           <svg className="absolute -left-[6px] top-[56px]" xmlns="http://www.w3.org/2000/svg" width="7" height="13" viewBox="0 0 7 13" fill="none">
             <path d="M2.5 10.831C-0.833337 8.90653 -0.833336 4.09528 2.5 2.17078L6.25 0.00571251V12.9961L2.5 10.831Z" fill="black"/>
           </svg>
